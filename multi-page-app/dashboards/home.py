cf6869267c7870c6463ad960aaf16fb8855b5a66
rgalbo <rcgalbo@gmail.com>
2020-03-30 11:36:18 -0400

    fix breaking change add bootstrap

--- multi-page-app/dashboards/home.py
+++ multi-page-app/dashboards/home.py
@@ -6,26 +6,28 @@ from app import app
 
 from app import hospitals
 # define homepage layout
-layout = html.Div(id='contianer'
+layout = html.Div(
     [
         html.H3('Home Page - US Hospital Data'),
         html.P('Select state from dropdown to get the number of hospitals'),
+        html.Div(id='home-display-value', className='col-8'),
         dcc.Dropdown(
-            id='home-dropdown',
-            options=[
-                {'label': '{}'.format(i), 'value': i}
-                for i in ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
-          "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
-          "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
-          "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
-          "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+        id='home-dropdown',
+        className='col-8',
+        options=[
+            {'label': '{}'.format(i), 'value': i}
+            for i in ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
+                    "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
+                    "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
+                    "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
+                    "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
             ],
         ),
-        html.Div(id='home-display-value'),
         dcc.Link('Go to dash1 - map', href='/dash1'),
         html.Div(),
         dcc.Link('Go to dash2 - data', href='/dash2'),
-    ]
+    ],
+    className='container'
 )
 
 
@@ -34,7 +36,7 @@ layout = html.Div(id='contianer'
 )
 def display_value(value):
     if value == None:
-        val = ''
+        val = 0
     else:
         val = hospitals[hospitals.STATE == value].shape[0]
     return 'The state you have selected has {} hospitals'.format(val)
